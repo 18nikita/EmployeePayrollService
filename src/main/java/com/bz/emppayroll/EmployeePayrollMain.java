0f4eff9ff559599616223a8ee5256bbfb9721c8b
unknown <rajputnikita5666@gmail.com>
2021-10-28 18:53:58 +0530

    Added:Create Employee payroll service

---
+++ src/main/java/com/bz/emppayroll/EmployeePayrollMain.java
@@ -0,0 +1,13 @@
+package com.bz.emppayroll;
+
+public class EmployeePayrollMain {
+
+	public static void main(String[] args) {
+		System.out.println("..WELCOME TO EMPLOYEE PAYROLL SYSTEM..");
+		EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+
+		employeePayrollService.readEmployeeDataFromConsole();
+		employeePayrollService.writeEmployeeDataInConsole();
+	}
+
+}
